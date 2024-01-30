d51b58b40479d49c55675a7be1d8e789de7226c8
Tobijue <tobias-juenger@web.de>
2024-01-30 17:56:28 +0100

    Projektdateien hinzufügen.

---
+++ Konsole/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int zeilen = 20;
+            int spalten = 40;
+            
+            //Zählerschleife für Zeilen
+            for (int x = 0; x < zeilen; x++)
+            { 
+                //Zählerschleifen für spalten
+                for (int y = 0; y < spalten; y++)
+                {
+                //Erster Durchlauf x = 0 , y = 0
+                    //Wenn in der ersten oder letzten Zeile, die Zeile mit "-" für Rahmenbegrenzung füllen
+                    if (x == 0 || x == zeilen - 1)
+                    {
+                        Console.Write("-");
+                    }
+                    //Wenn in der ersten oder letzten Zeile, die Zeile mit "|" für Rahmenbegrenzung füllen
+                    else if (y == 0 || y == spalten - 1)
+                    {
+                        Console.Write("|");
+                    }
+                    //Auffüllen der zwischenräume
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                
+                }
+                //Beginn der neuen Zeile
+                Console.Write("\n");
+            }
+            
+            Console.ReadKey();
+
+        }
+    }
+}
